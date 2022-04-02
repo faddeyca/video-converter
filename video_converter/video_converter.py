3eabf64e62df6d2317fdf116c36cb0af6707f5f6
fadg25 <fadg25@yandex.ru>
2022-04-02 12:31:46 +0500

    Frame extractor added

--- video_converter/video_converter.py
+++ video_converter/video_converter.py
@@ -1,1 +1,16 @@
+import cv2
+import os
 
+
+a = os.listdir()
+if "temp" in a:
+    os.rmdir("temp")
+os.makedirs("temp")
+vidcap = cv2.VideoCapture('input.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite(os.path.join(r"C:\Users\Fadg\source\repos\video_converter\temp", "%d.png" % count), image)
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
